9d7ed979b63670110eaf32e4f46332ad00c61a10
Mario <mariocapodanno@live.it>
2014-12-03 12:49:47 +0100

    Create Main.c

---
+++ standard/Main.c
@@ -0,0 +1,16 @@
+//librerie
+#include <stdio.h>
+#include <conio.h>
+
+// prototipi funzioni
+
+
+// inizio programma principale
+main(){
+
+       //dichiarazione variabili
+       
+       printf("Hello world!");
+       getch();
+}
+//funzioni
